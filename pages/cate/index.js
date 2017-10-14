8d13a907f038b43131323ec3b152ea98178243e7
shenlihua <1104753531@qq.com>
2017-10-14 19:30:02 +0800

    分类页面调整

--- pages/cate/index.js
+++ pages/cate/index.js
@@ -1,11 +1,17 @@
 // pages/cate/index.js
+const CHOOSE_STR = 'scroll'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    toView: 'scroll0',
+    defaultToView: 'scroll0',
+    showActive: 'scroll0',
+    active:[1,0,0,0],
+    curren_active:0,
+    local : ''
   },
 
   /**
@@ -26,9 +32,21 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this
+    var local = []
+    wx.createSelectorQuery().selectAll('.right-main').boundingClientRect(function (rects) {
+      rects.forEach(function (rect,index) {
+        local[index] = rect
+      })
+      if(local.length > 0){
+        //动态赋值
+        that.setData({
+          local: local
+        })
+      }
+    }).exec()
+    
   },
-
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -62,5 +80,46 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  /**
+   * 用户滚动列表事件
+   */
+  listScroll: function(e) {
+    var local = this.data.local
+    for (var i = local.length - 1; i >= 0; i--) {
+      
+      if (local[i].top < e.detail.scrollTop +2) {
+        var id = i
+        var active = this.data.active
+        var curren_active = this.data.curren_active //当前选中状态
+        active[curren_active] = 0;
+        active[id] = 1;
+        this.setData({
+          toView: e.target.dataset.to,
+          curren_active: id,
+          active: active,
+          defaultToView: local[i].id
+        })
+        break;
+      }
+    }
+    
+  },
+  /**
+   * 用户点击分类事件
+   */
+  toView: function(e) {
+    console.log(this.data.local)
+    var id = e.target.dataset.id
+    var active = this.data.active
+    var curren_active = this.data.curren_active //当前选中状态
+    
+    active[curren_active] =0 ;
+    active[id] = 1;
+    this.setData({
+      toView: e.target.dataset.to,
+      curren_active: id,
+      active: active
+    })
   }
 })
